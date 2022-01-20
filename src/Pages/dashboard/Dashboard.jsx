0ed6718631acbf42a1c40ab38855f75fdf34c057
Phuc Hoang <phuchoang1232000@gmail.com>
2022-01-20 15:25:06 +0700

    set up layout again

--- src/Pages/dashboard/Dashboard.jsx
+++ src/Pages/dashboard/Dashboard.jsx
@@ -1,21 +1,34 @@
 import React from "react";
-import Header from "../../components/Header";
-
 import Grid from "@mui/material/Grid";
+import { makeStyles } from "@mui/styles";
+
+import Header from "../../components/Header";
 import MenuLeft from "./components/MenuLeft";
+
+const useStyles = makeStyles({
+  dashboard: {
+    backgroundColor: "#f0f2f5",
+  },
+});
 export default function Dashboard() {
+  const classes = useStyles();
   return (
     <div>
       <Header />
       {/* <MenuLeft /> */}
-      <Grid container fluid spacing={0.3}>
-        <Grid item xs={2.4}>
+      <Grid
+        className={classes.dashboard}
+        sx={{ mt: "64px" }}
+        container
+        spacing={0}
+      >
+        <Grid item xs={2.684}>
           <MenuLeft />
         </Grid>
         <Grid item xs>
           <MenuLeft />
         </Grid>
-        <Grid item xs={2.4}>
+        <Grid item xs={2.684}>
           <MenuLeft />
         </Grid>
       </Grid>
---
+++ src/Pages/dashboard/components/MenuRight/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './menuRight.scss'
+
+MenuRight.propTypes = {
+    
+};
+
+function MenuRight(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default MenuRight;
