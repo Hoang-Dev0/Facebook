8e03632592ab95adf2afedff85eabe35044096f8
Phuc Hoang <phuchoang1232000@gmail.com>
2022-02-07 09:27:38 +0700

    fix UI header

--- src/commons/Header/components/accountSetting/AccountSetting.jsx
+++ src/commons/Header/components/accountSetting/AccountSetting.jsx
@@ -1,40 +1,42 @@
 import React from "react";
 import "./accountsetting.scss";
 import ExitToAppIcon from "@mui/icons-material/ExitToApp";
-import NightlightIcon from '@mui/icons-material/Nightlight';
-import HelpIcon from '@mui/icons-material/Help';
-import SettingsIcon from '@mui/icons-material/Settings';
-import AnnouncementIcon from '@mui/icons-material/Announcement';
+import NightlightIcon from "@mui/icons-material/Nightlight";
+import HelpIcon from "@mui/icons-material/Help";
+import SettingsIcon from "@mui/icons-material/Settings";
+import AnnouncementIcon from "@mui/icons-material/Announcement";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function AccountSetting(props) {
-    const [logout, setLogout] = useState("");
-    const navigate = useNavigate(); 
+  const [logout, setLogout] = useState("");
+  const navigate = useNavigate();
 
-    const handleLogOut =()=>{
-        localStorage.removeItem("loggedUser");
-        setTimeout(() => {
-            navigate("/login");
-          }, 2000);
-    }
+  const handleLogOut = () => {
+    localStorage.removeItem("loggedUser");
+    setTimeout(() => {
+      navigate("/login");
+    }, 1000);
+  };
   return (
     <div className="accountSetting">
       <div className="accountSetting__info">
-          <div className="accountSetting__info-avatar">
-              <img src={props.currentUser["avatar"]} alt="" />
-          </div>
-          <div className="accountSetting__info-name">{props.currentUser["name"]}</div>
-          <div className="accountSetting__info-title">Xem trang cá nhân của bạn</div>
+        <div className="accountSetting__info-avatar">
+          <img src={props.currentUser["avatar"]} alt="" />
+        </div>
+        <div className="accountSetting__info-name">
+          {props.currentUser["name"]}
+        </div>
+        <div className="accountSetting__info-title">
+          Xem trang cá nhân của bạn
+        </div>
       </div>
       <hr className="accountSetting__line" />
       <div className="accountSetting__opinion">
         <div className="accountSetting__opinion-icon">
           <AnnouncementIcon sx={{ fontSize: 20 }} />
         </div>
-        <div className="accountSetting__opinion-title">
-          Đóng góp ý kiến
-        </div>
+        <div className="accountSetting__opinion-title">Đóng góp ý kiến</div>
       </div>
       <hr className="accountSetting__line" />
       <div className="accountSetting__item">
@@ -62,7 +64,10 @@ export default function AccountSetting(props) {
             Màn hình & trợ năng
           </div>
         </div>
-        <div onClick={()=>handleLogOut()} className="accountSetting__item-logout">
+        <div
+          onClick={() => handleLogOut()}
+          className="accountSetting__item-logout"
+        >
           <div className="accountSetting__item-logout--icon">
             <ExitToAppIcon sx={{ fontSize: 20 }} />
           </div>
